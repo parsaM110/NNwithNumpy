61e725bed8ad01f3fbd0b73a648ab46d99dfae03
parsaM110 <pmomeni80@gmail.com>
2023-04-23 01:02:46 +0330

    second neuron

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-inputs = [1.2, 5.1, 2.1] # 3 input/neuron -> 3 weight
-weights = [3.1, 2.1, 8.7] # every unique neuron has unique bias
-bias = 3
+inputs = [1, 2, 3, 2.5] # 3 input/neuron -> 3 weight
+weights = [0.2, 0.8, -0.5, 1.0] # every unique neuron has unique bias
+bias = 2
 
 
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
 print(output)
