5aca2e579dfece3d9e51e15ff6f19de6fbe6b2fc
parsaM110 <pmomeni80@gmail.com>
2023-09-06 11:44:43 +0330

    changing not remember exactly what

--- p4.py
+++ p4.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]] # 3 * 4
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-          [0.5, -0.91, 0.26, -0.5],
-          [-0.26, -0.27, 0.17, 0.87]]  # 3 * 4
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]] 
 
-biases = [2, 3, 0.5]
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((n_neurons,))
 
-weights2 = [[0.1, -0.14, 0.5],
-          [-0.5, 0.12, -0.33],
-          [-0.44, 0.73, -0.13]]  # 3 * 4
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-biases2 = [-1, 2, -0.5]
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
 
-# output = np.dot(inputs, np.transpose(weights) ) + biases # |-> or
-layer1_outputs = np.dot(inputs, np.array(weights).T ) + biases
+layer1.forward(X)
+#print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
 
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T ) + biases2
-
-print(layer2_outputs)
+#print(0.10 * np.random.randn(4, 3))
 
